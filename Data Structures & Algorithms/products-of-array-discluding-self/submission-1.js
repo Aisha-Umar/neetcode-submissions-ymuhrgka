55553c7664de2b60d22193d0232eaadf4b8b44e6
Aisha Umar <igotwebmail@gmail.com>
2026-06-27 18:08:02 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+      let output = []
+      let left = 1
+      let right = 1
+
+      for(let i=0; i < nums.length; i++){
+        output[i]=left
+        left *= nums[i]
+      }
+      for(let i = nums.length-1; i >= 0; i--){
+        output[i] *= right
+        right *= nums[i]
+      }
+      return output
+    }
+
+}
